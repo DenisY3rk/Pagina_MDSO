51ea2cb0210e744ee03187905252153710b4e2ee
DenisY3rk2 <39835145+DenisY3rk2@users.noreply.github.com>
2021-07-12 12:25:45 -0500

    menu v1

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Municipalidad Distrital de San Juan del Oro</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap-theme.css">
+    <link rel="stylesheet" href="css/estilo.css">
+    <script src="js/bootstrap.js"></script>
+</head>
+
+<body>
+    <body>
+    <header>
+      <div class="header-content">
+          <div class="logo">
+            <ul>
+            <li><img src="img/logo1.png" class="logo_img"></li>
+            <li><a href="index.php" class="logo_a">Municipalidad Distrital de San Juan del Oro</a>
+          </div>
+      
+          <div class="menu">
+            <nav>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li class="lista_item list_item--click"><a href="#">Municipio</a>
+                        <ul>
+                            <li><a href="#">Misión</a></li>
+                            <li><a href="#">Visión</a></li>
+                            <li><a href="#">Organigrama</a></li>
+                        </ul>
+                    </li>
+
+                    <li><a href="#">Noticias</a></li>
+                    <li><a href="#">Trámite documentario</a></li> 
+                </ul>
+            </nav>
+
+        
+          </div>
+      </div>
+    </header>
+  </body>
+</html>
